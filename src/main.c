d5aeb49a1552fc7537f6e5580c34defab47b97fc
luvimsan <luvimsan@gmail.com>
2026-05-13 16:32:26 +0300

    chore(style): style the code output

--- src/main.c
+++ src/main.c
@@ -11,18 +11,19 @@ void print_arr(int nums[], int n) {
 int main() {
     int nums1[] = {1, 5, 1, 1, 6, 4};
     int n1 = sizeof(nums1) / sizeof(nums1[0]);
-    printf("Original array: ");
+    printf("Original array\n");
     print_arr(nums1, n1);
     wiggle_sort_nonrecursive(nums1, n1);
-    printf("Wiggled sorted array: ");
+    printf("Wiggled sorted array\n");
     print_arr(nums1, n1);
+    printf("\n");
 
     int nums2[] = {1, 3, 2, 2, 3, 1};
     int n2 = sizeof(nums2) / sizeof(nums2[0]);
-    printf("Original array: ");
+    printf("Original array\n");
     print_arr(nums2, n2);
     wiggle_sort_nonrecursive(nums2, n2);
-    printf("Wiggled sorted array: ");
+    printf("Wiggled sorted array\n");
     print_arr(nums2, n2);
 
     return 0;
